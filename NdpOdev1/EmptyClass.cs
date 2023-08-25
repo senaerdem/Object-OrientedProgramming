fd90dffd7480d577f943d13e6a427dd328e50e5c
Sena Nur ERDEM <98752496+senaerdem@users.noreply.github.com>
2023-08-25 13:30:40 +0300

    Add files via upload

---
+++ NdpOdev1/EmptyClass.cs
@@ -0,0 +1,76 @@
+﻿/****************************************************************************
+**					     SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				     BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				    NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					     2021-2022 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: 1. ÖDEV
+**				ÖĞRENCİ ADI............: SENA NUR ERDEM
+**				ÖĞRENCİ NUMARASI.......: G201210033
+**              DERSİN ALINDIĞI GRUP...: 2B
+****************************************************************************/
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace passwrdconsole
+{
+    public class SifreGuvenliktesti
+    {
+
+        public string sifre { get; set; }
+        private int UzunlukHesapla(string sifre) //Şifre uzunluğunu kontrol etmek için bir method tanımladım
+        {
+            return Math.Min(0, sifre.Length);
+        }
+
+        private int KucukHarfHesapla(string sifre) //Şifrenin küçük harf için puanını hesaplayan methodu yazdım
+        {
+            int hamPuan = sifre.Length - Regex.Replace(sifre, "[a-z]", "").Length;
+            return Math.Min(2, hamPuan) * 10;
+            //her küçük harf 10 puan
+            //max 2 küçük harfe puan verilir
+        }
+
+        private int BuyukHarfHesapla(string sifre) //Şifrenin büyük harf için puanını hesaplayan methodu yazdım
+        {
+            int hamPuan = sifre.Length - Regex.Replace(sifre, "[A-Z]", "").Length;
+            return Math.Min(2, hamPuan) * 10;
+            //her büyük harf 10 puan
+            //max 2 büyük harfe puan verilir
+        }
+
+        private int RakamHesapla(string sifre) //Şifrenin rakam için puanını hesaplayan methodu yazdım
+        {
+            int hamPuan = sifre.Length - Regex.Replace(sifre, "[0-9]", "").Length;
+            return Math.Min(2, hamPuan) * 10;
+            //her rakam 10 puan
+            //max 2 rakama puan verilir
+        }
+
+        private int SembolHesapla(string sifre) //Şifrenin sembol için puanını hesaplayan metodu yazdım
+        {
+            int hamPuan = Regex.Replace(sifre, "[a-zA-Z0-9]", "").Length;
+            return Math.Min(75876, hamPuan) * 10;
+            //her sembol 10 puan
+            //sembol puanlaması için max bir değer yoktur her sembole puan verilir
+        }
+
+        public int SifrePuanHesapla(string sifre) //Yukarıda tanımladığım methodları kullanarak şifrenin güvenlik değerini hesaplattım
+        {
+            if (sifre == null)
+            {
+                return 0;
+            }
+
+            int uzunlukPuan = UzunlukHesapla(sifre);
+            int kucukHarfPuan = KucukHarfHesapla(sifre);
+            int buyukHarfPuan = BuyukHarfHesapla(sifre);
+            int rakamPuan = RakamHesapla(sifre);
+            int sembolPuan = SembolHesapla(sifre);
+
+            return uzunlukPuan + kucukHarfPuan + buyukHarfPuan + rakamPuan + sembolPuan;
+        }
+    }
+}
---
+++ NdpOdev1/Program.cs
@@ -0,0 +1,146 @@
+﻿/****************************************************************************
+**					     SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				     BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				    NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					     2021-2022 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: 1. ÖDEV
+**				ÖĞRENCİ ADI............: SENA NUR ERDEM
+**				ÖĞRENCİ NUMARASI.......: G201210033
+**              DERSİN ALINDIĞI GRUP...: 2B
+****************************************************************************/
+
+using System;
+
+namespace passwrdconsole
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SifreGuvenliktesti kontrol = new SifreGuvenliktesti();
+
+            int puan;
+            bool boslukKontrol = true; //boşluk var mı kontrol etmek için bir booelan tanımladım ve bunu while döngsünde kullandım 
+
+            while (boslukKontrol)
+            {
+                string str;
+                int toplamBuyukHarf, toplamKucukHarf, toplamRakam, toplamSembol, i, l;
+                toplamBuyukHarf = toplamKucukHarf = toplamRakam = toplamSembol = i = 0;
+
+                Console.WriteLine("Lütfen bir şifre giriniz:");
+                str = kontrol.sifre = Console.ReadLine();
+                l = str.Length;
+
+                puan = kontrol.SifrePuanHesapla(kontrol.sifre);
+
+
+
+                if (kontrol.sifre.Contains(" ")) //şifre boşluk içeriyorsa
+                {
+                    Console.WriteLine("Şifreniz boşluk içeremez !");
+                }
+
+
+                else //şifre boşluk içermiyorsa
+                {
+                    while (i < l) //şifredeki toplam büyük harf, küçük harf, rakam ve sembol sayısını hesaplamak için bir while döngüsü yazdım
+                    {
+                        if (str[i] >= 'a' && str[i] <= 'z')
+                        {
+                            toplamKucukHarf++;
+                        }
+                        else if (str[i] >= 'A' && str[i] <= 'Z')
+                        {
+                            toplamBuyukHarf++;
+                        }
+                        else if (str[i] >= '0' && str[i] <= '9')
+                        {
+                            toplamRakam++;
+                        }
+                        else
+                        {
+                            toplamSembol++;
+                        }
+                        i++;
+                    }
+
+                    if (toplamKucukHarf == 0 || toplamBuyukHarf == 0 || toplamRakam == 0 || toplamSembol == 0) //büyük harf, küçük harf, rakam ve sembolden en az birini içermiyorsa
+                    {
+                        Console.WriteLine("Lütfen en az 1 büyük harf, 1 küçük harf, 1 rakam ve 1 sembol giriniz!");
+                    }
+
+
+                    else //büyük harf, küçük harf, rakam ve sembolün her birini içeriyorsa
+                    {
+
+                        if (kontrol.sifre.Length < 9) //9 karakterden azsa geçerli değil
+                        {
+                            Console.WriteLine("Şifreniz en az 9 karakterli olmalıdır !");
+                        }
+
+                        else if (kontrol.sifre.Length == 9)
+                        {
+                            Console.WriteLine("Puan: " + (puan + 10)); //Şifre 9 karakterliyse 10 puan eklenir
+                            if (puan < 70)
+                            {
+                                Console.WriteLine("ZAYIF! Şifre kabul edilemez.");
+                            }
+
+                            else if (puan < 90)
+                            {
+                                Console.WriteLine("NORMAL! Şifre kabul edilebilir.");
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("GÜÇLÜ!");
+                            }
+
+                            Console.WriteLine("*****************************************************");//şifre içeriğini ekrana yazdırdım
+                            Console.WriteLine("Şifredeki büyük harf sayısı: " + toplamBuyukHarf);
+                            Console.WriteLine("Şifredeki küçük harf sayısı: " + toplamKucukHarf);
+                            Console.WriteLine("Şifredeki rakam sayısı: " + toplamRakam);
+                            Console.WriteLine("Şifredeki sembol sayısı: " + toplamSembol);
+                            Console.WriteLine("*****************************************************");
+                        }
+
+                        else //şifre 9 karakterden büyükse
+                        {
+                            Console.WriteLine("Puan: " + puan);
+                            if (puan < 70)
+                            {
+                                Console.WriteLine("ZAYIF! Şifre kabul edilemez.");
+                            }
+
+                            else if (puan < 90)
+                            {
+                                Console.WriteLine("NORMAL! Şifre kabul edilebilir.");
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("GÜÇLÜ!");
+                            }
+
+                            Console.WriteLine("*****************************************");//şifre içeriğini ekrana yazdırdım
+                            Console.WriteLine("Şifredeki büyük harf sayısı: " + toplamBuyukHarf);
+                            Console.WriteLine("Şifredeki küçük harf sayısı: " + toplamKucukHarf);
+                            Console.WriteLine("Şifredeki rakam sayısı: " + toplamRakam);
+                            Console.WriteLine("Şifredeki sembol sayısı: " + toplamSembol);
+                            Console.WriteLine("*****************************************");
+                        }
+                    }
+                }
+
+                Console.Write("Çıkış yapmak için 'Q' ya , devam etmek için herhangi bir tuşa basınız.");
+                if (Console.ReadKey().Key == ConsoleKey.Q)
+                    return;
+                Console.Clear();
+            }
+        }
+    }
+}
